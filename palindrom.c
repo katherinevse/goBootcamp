0681c27bf34eb06da01c1a8533f05fc0eb0bc1cd
Katherine <katherinevse@mail.ru>
2023-11-28 17:10:46 +0300

    all_functions done

---
+++ palindrom.c
@@ -0,0 +1,13 @@
+bool isPalindrome(int x){
+    int result =0,sum=0,num=x;
+    while(x>0){
+        result = x % 10;  // result = 1
+        sum = sum * 10 + result;   // sum = 1
+        x /= 10;            // x = 12
+    }
+
+    if(sum==num)
+    return true;
+    else
+    return false;
+}
