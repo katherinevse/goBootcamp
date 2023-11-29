ed6899dfac2d5b8df997bb4dc79e017216c4413d
Loris Pamula <lorispam@il-k5.msk.21-school.ru>
2023-11-29 11:41:21 +0300

    -

--- remove_el.c
+++ remove_el.c
@@ -1,15 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 int removeElement(int* nums, int numsSize, int val) {
+    int left = 0;
+    int right = 0;
+
+    while(right < numsSize){
+        if(nums[right]== val){
+            right++;
+        }
+        else
+        {
+            nums[left] = nums[right];
+            right++;
+            left++;
+        }
+        
+        
     
+    }
 
-
-
-    
+    return left;
 }
 
-
 int main(){
+    int nums[] = {3,2,2,3};
+    int val = 3;
+    int nSize = 4;
 
 
-    
+    removeElement(nums, nSize, val);
+    return 0;
 }
