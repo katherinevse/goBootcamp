47bd69fdb906ef7f2f4c7bbb650150d606f1a7cd
Loris Pamula <lorispam@il-k5.msk.21-school.ru>
2023-11-29 14:07:05 +0300

    -

---
+++ longestCommonPrefix.c
@@ -0,0 +1,20 @@
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+
+
+
+
+
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+
+
+    
+}
+
+
+int main(){
+
+}
