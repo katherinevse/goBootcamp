5e055a2dc236ba56daac46243d3b90f6d9b95c77
Loris Pamula <lorispam@il-k5.msk.21-school.ru>
2023-11-29 12:17:08 +0300

    -

---
+++ max_consec.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+    int lenght = 0;
+    int newLenght = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        if(nums[i] == 1){
+            newLenght++;
+        }
+        if(nums[i] != 1){
+            newLenght = 0;
+        }
+        if(newLenght > lenght)
+        lenght = newLenght;
+
+    }
+    return lenght;
+
+}
+
+
+int main(){
+    int nums[] = {1,1,0,1,1,1};
+    int nsize = 6;
+    printf("%d", findMaxConsecutiveOnes(nums,nsize));
+
+    return 0;
+}
