8bb8d669a881ac0166d7473d548368748af43325
Katherine <katherinevse@mail.ru>
2023-12-07 15:47:30 +0300

    -

--- Easy/Java/largest_odd.java
+++ Easy/Java/largest_odd.java
@@ -1,10 +1,15 @@
+//substring - это метод в Java, который используется для извлечения подстроки из строки
+
 class Solution {
     public String largestOddNumber(String num) {
-
-        if (num.length()-1) {
-            
+        for (int i = num.length()-1; i >= 0; i--) {
+            int current=num.charAt(i);
+            if (current%2 !=0) {
+                return num.substring(0,i+1);
+                
+            }
         }
-
+        return "";
         
     }
 }
