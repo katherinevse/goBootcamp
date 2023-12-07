49f869b160ec60b41beaba1e571ee32f8c4bc060
Katherine <katherinevse@mail.ru>
2023-12-07 14:55:45 +0300

    -

--- Easy/Java/largest_odd.java
+++ Easy/Java/largest_odd.java
@@ -1,5 +1,10 @@
 class Solution {
     public String largestOddNumber(String num) {
+
+        if (num.length()-1) {
+            
+        }
+
         
     }
 }
--- Easy/Java/lengthOfLastWord.java
+++ Easy/Java/lengthOfLastWord.java
@@ -1,15 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String str = s.trim();
+        String str = s.trim(); //удаление пробелов
         int count = 0;
         for(int i=str.length()-1;i>=0;i--){
-            if(str.charAt(i) != ' '){
+            if(str.charAt(i) != ' '){ //посимвольно считывает 
                 count++;
             }
             else{
                 break; 
             }
-        
         }
         return count;
     }
