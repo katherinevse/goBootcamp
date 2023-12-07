9df05eee9a3603502511af0668d8865d29c2d2c1
Katherine <katherinevse@mail.ru>
2023-12-07 13:22:02 +0300

    -

--- longestCommonPrefix.c
+++ longestCommonPrefix.c
@@ -1,20 +1,40 @@
-// Input: strs = ["flower","flow","flight"]
-// Output: "fl"
-// Write a function to find the longest common prefix string amongst an array of strings.
-// If there is no common prefix, return an empty string "".
-
-
-
-
-
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
 
 char* longestCommonPrefix(char** strs, int strsSize) {
+    char **ptrToStrs = strs;
 
+    for (int i = 0; i < strsSize; i++)
+    {
+        if(strncmp(ptrToStrs[i], ptrToStrs[i+1], 1) != 0)
+            printf("Первые символы в словах разные.\n");
+            return NULL;
 
-    
+    }
+    printf("Первые символы в каждом слове одинаковы.\n");
+    char *result = malloc(n);
+    result[0] = ptrToStrs[0][0];
+    result[1] = '\0';
+
+    return result;
+
 }
-
 
 int main(){
+    char *strs[] = {"flower", "flow"};
+    int n = 2;
+    char *commonPrefix = longestCommonPrefix(strs, n);
+
+    if (commonPrefix != NULL) {
+        printf("Общий префикс: %s\n", commonPrefix);
+        free(commonPrefix); 
+    }
+
+    return 0;
 
 }
+
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
--- palindrom.c
+++ palindrom.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-bool isPalindrome(int x) {
-  int result = 0, sum = 0, num = x;
-  while (x > 0) {
-    result = x % 10;          // result = 1
-    sum = sum * 10 + result;  // sum = 1
-    x /= 10;                  // x = 12
-  }
+bool isPalindrome(int x){
+int t=x,b,s; //t = 121
+while(x>0){
+    b=x%10; // 1
+    s=s*10+b;
+    x/=10;
+}
+if(s==t)
+  return true;
+else 
+  return false;
+}
 
-  if (sum == num)
-    return true;
-  else
-    return false;
+int main(){
+  int val =  121;
+  isPalindrome(val);
+
+  return 0;
+
 }
