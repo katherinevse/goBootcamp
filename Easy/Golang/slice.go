9c6fa42baf116dbdca0c27afb5e02e70bfaf3dee
Katherine <katherinevse@mail.ru>
2024-01-15 17:48:31 +0500

    -

--- Easy/Golang/slice.go
+++ Easy/Golang/slice.go
@@ -12,6 +12,11 @@ func main() {
 	fmt.Println("Длина списка: ", len(todoList))
 	fmt.Println("Емкость списка: ", cap(todoList))
 
+	todoList = append(todoList, "Сходить к бабушке")
+
+	fmt.Println("Длина списка после: ", len(todoList))
+	fmt.Println("Емкость списка после: ", cap(todoList))
+
 	for index, item := range todoList {
 		fmt.Printf("%d %s\n", index+1, item)
 
