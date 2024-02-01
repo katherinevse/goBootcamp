ea6f7dd691e83e07081a2db2ca2ea88a40606224
Katherine <katherinevse@mail.ru>
2024-02-01 12:28:44 +0300

    http

--- Golang2/3.Trash_API/main.go
+++ Golang2/3.Trash_API/main.go
@@ -1,8 +1,10 @@
-package mainтт
+package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -13,33 +15,33 @@ var (
 )
 
 func init() {
-	pizza1:= Pizza{
-		ID: 1,
+	pizza1 := Pizza{
+		ID:       1,
 		Diameter: 22,
-		Price: 500,
-		Title: "Margarita"
+		Price:    500,
+		Title:    "Margarita",
 	}
-	pizza2:= Pizza{
-		ID: 2,
+	pizza2 := Pizza{
+		ID:       2,
 		Diameter: 22,
-		Price: 550,
-		Title: "BBQ"
+		Price:    550,
+		Title:    "BBQ",
 	}
-	pizza2:= Pizza{
-		ID: 3,
+	pizza3 := Pizza{
+		ID:       3,
 		Diameter: 22,
-		Price: 700,
-		Title: "Pepperooni"
+		Price:    700,
+		Title:    "Pepperooni",
 	}
-	db = append(db, pizza1,pizza2,pizza3)
+	db = append(db, pizza1, pizza2, pizza3)
 }
 
 // наша модель
 type Pizza struct {
-	ID       int `json:"id"`
-	Diameter int `json:"diameter"`
-	Price    int `json:"price"`
-	Title    int `json:"title"`
+	ID       int    `json:"id"`
+	Diameter int    `json:"diameter"`
+	Price    int    `json:"price"`
+	Title    string `json:"title"`
 }
 
 // вспомогательная функция для модели  (модельный метод)
@@ -52,15 +54,34 @@ func FindPizzaById(id int) (Pizza, bool) {
 			found = true
 			break
 		}
-		return pizza, found
+	}
+	return pizza, found
+
+}
+
+func GetAllPizzas(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	log.Println("Кто-то вызвал данный запрос обо всех пиццах")
+	w.WriteHeader(200)            // StatusCode для запроса ЗАЧЕМ ЭТО ПРОПИСЫВАТЬ?
+	json.NewEncoder(w).Encode(db) //Сериализация+ запись в w NewEncoder- подготавливает экземляр записи Encode- записывает в байты, записывает в формате json в w, после этого w закрывается
+}
+
+func GetPizzaById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	// считаем id из строки запроса и конвертируем его в инт
+	//получаем значение из id
+	vars := mux.Vars(r)                 // извлекает переменные маршрута из объекта http.Request.{id} - это переменная маршрута, эти значения могут быть извлечены с использованием mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"]) // отдаем то, что лежит в поле id
+	if err != nil {
+		log.Fatal(err) // пока что так
+	}
+	log.Println("Trying to send to client pizza with id", id)
+	pizza, ok := FindPizzaById(id)
+	if ok {
+		w.WriteHeader(200)
 	}
 
 }
-
-func GetAllPizzas(w http.ResponseWriter, request *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-}
-func GetPizzaById(w http.ResponseWriter, request *http.Request) {}
 
 func main() {
 	log.Println("Trying to start REST API pizza!")
